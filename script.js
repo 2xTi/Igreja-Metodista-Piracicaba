049ae2d44543f9a2e1c885e8e912f7094d11fe95
Tiago Reis <treisalmeida@gmail.com>
2022-12-21 19:05:21 -0300

    nao deu certo

--- script.js
+++ script.js
@@ -3,55 +3,70 @@ const firstname = document.getElementById('firstname');
 const lastname = document.getElementById('lastname');
 const email = document.getElementById('email');
 const number = document.getElementById('number');
+var test = 0;
 
-form.addEventListener('submit', (e) =>{
-    e.preventDefault();
-    checkInputs();
+form.addEventListener('submit', (e) => {
+   e.preventDefault();
+   checkInputs();
 })
 
-function checkInputs(){
-    const firstnameValue = firstname.value.trim()
-    const lastnameValue = lastname.value.trim()
-    const emailValue = email.value.trim()
-    const numberValue = number.value.trim()
-    
+function checkInputs() {
+   const firstnameValue = firstname.value.trim()
+   const lastnameValue = lastname.value.trim()
+   const emailValue = email.value.trim()
+   const numberValue = number.value.trim()
 
-    if(firstnameValue === '') {
-        errorValidation(firstname)
-     } else {
-        successValidation(firstname)
-     } 
-     if(lastnameValue === '') {
-        errorValidation(lastname)
-     } else {
-        successValidation(lastname)
-     } 
-     if(emailValue === '') {
-        errorValidation(email)
-     } else {
-        successValidation(email)
-     } 
-     if(numberValue === '') {
-        errorValidation(number)
-     }else if(numberValue.length<11){
-        errorValidation(number)
-     }
-      else {
-        successValidation(number)
-     } 
+   if (firstnameValue === '') {
+      errorValidation(firstname)
+   } else {
+      successValidation(firstname)
+      test + 1;
+   }
+   if (lastnameValue === '') {
+      errorValidation(lastname)
+   } else {
+      successValidation(lastname)
+      test + 1;
+   }
+   if (emailValue === '') {
+      errorValidation(email)
+   } else {
+      successValidation(email)
+      test + 1;
+   }
+   if (numberValue === '') {
+      errorValidation(number)
+   } else if (numberValue.length < 11) {
+      errorValidation(number)
+   }
+   else {
+      successValidation(number)
+      test + 1;
+   }
+
+   if (test === 4) {
+      changeDiv()
+   }
 }
 
 
-function errorValidation(input){
+function changeDiv(input) {
 
-    const inputBox = input.parentElement; 
+   const formChange = input.parentElement;
+   formChange.className = 'newForm'
+}
 
-    inputBox.className = 'input-box error'
+function errorValidation(input) {
+
+   const inputBox = input.parentElement;
+
+   inputBox.className = 'input-box error'
 }
 
 
-function successValidation (input){
+function successValidation(input) {
 
-    const formControl = input.parentElement;
-    formControl.className = 'input-box success'
+   const formControl = input.parentElement;
+   formControl.className = 'input-box success'
 }
+
