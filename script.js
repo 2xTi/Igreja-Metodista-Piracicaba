211b94ca6850eca4667ca222ef8a75de6dbaaefe
Tiago Reis <treisalmeida@gmail.com>
2022-12-22 14:36:36 -0300

    trabalho pi etapa 2 completo

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
+
 const form = document.getElementById('form');
 const firstname = document.getElementById('firstname');
 const lastname = document.getElementById('lastname');
 const email = document.getElementById('email');
 const number = document.getElementById('number');
-var test = 0;
+const formH = document.getElementById('form-header');
+const radioS = document.getElementById('sim');
+const radioN = document.getElementById('nao');
+const radioNone = document.getElementById('none');
+const checkS = document.getElementById('csim');
+const checkN = document.getElementById('cnao');
+const checkNoite = document.getElementById('cnoite');
+const editar = document.getElementById('editar');
+const verifica = document.getElementById('verificar');
 
-form.addEventListener('submit', (e) => {
+function validateEmail(email) {
+   var re = /\S+@\S+\.\S+/;
+   return re.test(email);
+}
+
+verifica.addEventListener('click', (e) => {
    e.preventDefault();
    checkInputs();
 })
@@ -20,19 +34,17 @@ function checkInputs() {
       errorValidation(firstname)
    } else {
       successValidation(firstname)
-      test + 1;
    }
    if (lastnameValue === '') {
       errorValidation(lastname)
    } else {
       successValidation(lastname)
-      test + 1;
    }
-   if (emailValue === '') {
+
+   if (validateEmail(email.value) == false) {
       errorValidation(email)
    } else {
       successValidation(email)
-      test + 1;
    }
    if (numberValue === '') {
       errorValidation(number)
@@ -41,20 +53,53 @@ function checkInputs() {
    }
    else {
       successValidation(number)
-      test + 1;
    }
 
-   if (test === 4) {
-      changeDiv()
+   if (firstnameValue !== '' && lastnameValue !== '' && validateEmail(emailValue) !== false && numberValue !== '' && numberValue > 11) {
+
+      document.getElementById('allPrint').classList.remove('hide')
+      document.getElementById('fnome-js').innerHTML = firstname.value;
+      document.getElementById('lnome-js').innerHTML = lastname.value;
+      document.getElementById('email-js').innerHTML = email.value;
+      document.getElementById('number-js').innerHTML = number.value;
+
+      document.getElementById('editar').classList.remove('hide')
+      document.getElementById('limpar').classList.remove('hide')
+      document.getElementById('enviar').classList.remove('hide')
+      document.getElementById('verificar').classList.add('hide')
+
+      firstname.readOnly = true
+      lastname.readOnly = true
+      email.readOnly = true
+      number.readOnly = true
+      radioS.disabled = true
+      radioN.disabled = true
+      radioNone.disabled = true
+      checkS.disabled = true
+      checkN.disabled = true
+      checkNoite.disabled = true
+
+      editar.addEventListener('click', (e) => {
+         e.preventDefault();
+         firstname.readOnly = false
+         lastname.readOnly = false
+         email.readOnly = false
+         number.readOnly = false
+         radioS.disabled = false
+         radioN.disabled = false
+         radioNone.disabled = false
+         checkS.disabled = false
+         checkN.disabled = false
+         checkNoite.disabled = false
+         document.getElementById('editar').classList.add('hide')
+         document.getElementById('limpar').classList.add('hide')
+         document.getElementById('enviar').classList.add('hide')
+         document.getElementById('verificar').classList.remove('hide')
+      });
+
    }
 }
 
-
-function changeDiv(input) {
-
-   const formChange = input.parentElement;
-   formChange.className = 'newForm'
-}
 
 function errorValidation(input) {
 
