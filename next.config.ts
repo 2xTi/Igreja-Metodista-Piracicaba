58d74c00afd38f79aa4d3211f268e86e61b68262
2xTi <treislameida@gmail.com>
2025-11-24 18:47:39 -0300

    Final cleanup: update next.config for Vercel deployment

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,6 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === 'production';
-
 const nextConfig: NextConfig = {
-  output: "export",
-  basePath: isProd ? "/Igreja-Metodista-Piracicaba" : "",
   images: {
     unoptimized: true,
   },
